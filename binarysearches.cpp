a379213c6aa1af2695f3b6fbdf4961d578abe392
Gian <36643266+gpena1@users.noreply.github.com>
2022-05-08 02:29:35 -0400

    Create binarysearches.cpp

---
+++ binarysearches.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int first_greq(vector<int>& x, int target){
+    int left=0,right=x.size()-1;
+    while(left<right){
+        int mid=left+(right-left)/2;
+        if(x[mid]>=target){
+            /*
+             * Since our middle element
+             * is greater than or equal to target,
+             * it is a possible candidate, but
+             * that does not rule out the elements
+             * to the left of the middle from being
+             * candidates as well.
+             */
+            right=mid;
+        }else{
+            /*
+             * At this point, we know x[mid]<target,
+             * which disqualifies it from
+             * being a candidate; we're looking for
+             * the first element >= to target, so
+             * we must move our left bound up one.
+             */
+            left=mid+1;
+        }
+    }
+    return left;
+}
+int first_gr(vector<int>& x, int target){
+    int left=0,right=x.size()-1;
+    while(left<right){
+        int mid=left+(right-left)/2;
+        if(x[mid]>target){
+            // logic from first_greq
+            // applies here, just change
+            // >= operator to >
+            right=mid;
+        }else{
+            // consult previous comment
+            left=mid+1;
+        }
+    }
+    return left;
+}
