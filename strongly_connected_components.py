45fd026cae6473819421c25d4a062fcfa11f38f9
Gian <flaconumero2@gmail.com>
2024-04-06 21:36:20 -0400

    Embarrasingly, the previous implementation of kosaraju's was completely incorrect; I don't know how I didn't notice it sooner. What is here now should be correct, but if anyone sees that it isn't, please let me know somehow.

--- strongly_connected_components.py
+++ strongly_connected_components.py
@@ -1,31 +1,46 @@
-from collections import defaultdict
-# graph should be given as a dictionary of outneighbors for each node
-# for ex, if 1 is said to have an outneighborhood of 2,3,5, then the
-# dictionary entry should look like 1: [2,3,5]
-def strongly_connected_components(graph,n):
-    L = []
-    visited = [False] * (n+1)
+# this code assumes that all edges are distinct
+# that is, if you have an edge (u,v) and another edge (a,b), they are
+# considered duplicates if u == a and v == b.
+out_neighbors = {
+    'a': ['f'],
+    'b': ['a','c','e'],
+    'c': ['f'],
+    'd': [],
+    'e': ['d'],
+    'f': ['b','e']
+}
+in_neighbors = {}
+for i,j in out_neighbors.items():
+    for k in j:
+        if k not in in_neighbors: in_neighbors[k] = []
+        in_neighbors[k].append(i)
 
-    def visit(u):
-        if not visited[u]:
-            visited[u] = True
-            for k in graph[u]:
-                visit(k)
-            L.append(u)
 
-    components = defaultdict(lambda: -1)
-    def assign(u,root):
-        if components[u] == -1:
-            components[u] = root
-            for k in graph[u]:
-                assign(k,root)
+v = set()
+ordering = []
+def visit(u):
+    if u in v: return
+    v.add(u)
+    for k in out_neighbors[u]:
+        visit(k)
+    ordering.append(u)
 
-    for i in graph.keys():
-        visit(i)
+components = {}
+def assign(u,v):
+    if u in components: return
+    components[u] = v
+    for k in in_neighbors[u]:
+        assign(k,v)
 
-    for i in L:
-        assign(i,i)
+for i in 'abcdef':
+    visit(i)
 
-    print('\n'.join(f'{i}: {j}' for i,j in components.items()))
+for i in ordering[::-1]:
+    assign(i,i)
 
-print(strongly_connected_components({1: [2], 2: [3], 3: [1,4], 4:[]}, 4))
+component_nodes = {}
+for i,j in components.items():
+    if j not in component_nodes: component_nodes[j] = []
+    component_nodes[j].append(i)
+
+print(component_nodes)
